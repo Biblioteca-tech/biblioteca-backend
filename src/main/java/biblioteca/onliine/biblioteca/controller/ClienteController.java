90c1bc1612268f90fe8b8befdff4c05085ffd233
Iarley Souza <iarleysouza615@gmail.com>
2025-09-14 17:43:07 -0300

    fix-adicionado envio de e-mails

--- src/main/java/biblioteca/onliine/biblioteca/controller/ClienteController.java
+++ src/main/java/biblioteca/onliine/biblioteca/controller/ClienteController.java
@@ -4,12 +4,14 @@ import biblioteca.onliine.biblioteca.dto.CadastroResponse;
 import biblioteca.onliine.biblioteca.model.Cliente;
 import biblioteca.onliine.biblioteca.repositories.UserRepository;
 import biblioteca.onliine.biblioteca.service.ConfigUser;
+import biblioteca.onliine.biblioteca.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Map;
 
 @RestController
@@ -17,11 +19,13 @@ import java.util.Map;
 public class ClienteController {
 
     ConfigUser configUser;
-
     UserRepository userRepository;
-    public ClienteController(UserRepository userRepository,  ConfigUser configUser) {
+    EmailService emailService;
+
+    public ClienteController(UserRepository userRepository, ConfigUser configUser,  EmailService emailService) {
         this.userRepository = userRepository;
         this.configUser = configUser;
+        this.emailService = emailService;
     }
 
     @PostMapping("/cadastro")
@@ -33,17 +37,22 @@ public class ClienteController {
             return cadastroResponse;
         }
         Cliente clienteSalvo = userRepository.save(cliente);
+
+        emailService.enviarEmailCadastro(clienteSalvo.getEmail(), clienteSalvo.getNome()); //enviar email.
+
         cadastroResponse.setSucesso(true);
         cadastroResponse.setMensagem("Cliente cadastrado com sucesso");
         cadastroResponse.setCliente(clienteSalvo);
         return cadastroResponse;
     }
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Map<String, String> loginRequest) {
         String email = loginRequest.get("email");
         String senha = loginRequest.get("senha");
 
         Cliente cliente = userRepository.findByEmail(email);
+
         if (cliente == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario não encontrado");
         }
@@ -51,7 +60,9 @@ public class ClienteController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body("Senha incorreta");
         }
+
         cliente.setSenha(null);
+        emailService.enviarEmailLogin(cliente.getEmail(), cliente.getNome());
         return ResponseEntity.status(HttpStatus.OK).body("Login realizado com sucesso");
 
     }
---
+++ src/main/java/biblioteca/onliine/biblioteca/service/EmailService.java
@@ -0,0 +1,35 @@
+package biblioteca.onliine.biblioteca.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void enviarEmailLogin(String destinatario, String nomeUsuario) {
+        SimpleMailMessage mensagem = new SimpleMailMessage();
+        mensagem.setTo(destinatario);
+        mensagem.setSubject("Login realizado com sucesso ✅");
+        mensagem.setText("Olá, " + nomeUsuario + "!\n\nSeu login foi realizado com sucesso em nossa plataforma.\n\nSe não foi você, entre em contato imediatamente.");
+        mensagem.setFrom("bibliotecaMedic@gmail.com");
+
+        mailSender.send(mensagem);
+    }
+
+    public void enviarEmailCadastro(String destinatario, String nomeUsuario) {
+        SimpleMailMessage mensagem = new SimpleMailMessage();
+        mensagem.setTo(destinatario);
+        mensagem.setSubject("Cadastro realizado com sucesso 🎉");
+        mensagem.setText("Olá, " + nomeUsuario + "!\n\nSeu cadastro foi realizado com sucesso em nossa plataforma!! Leia bastante livros e se divirta ao máximo.\n\nSe não foi você, entre em contato imediatamente.");
+        mensagem.setFrom("bibliotecaMedic@gmail.com");
+
+        mailSender.send(mensagem);
+    }
+
+}
