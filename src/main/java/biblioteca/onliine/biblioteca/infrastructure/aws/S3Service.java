ba4e3b671f1d9b63ad9fc09b815011f7ea321e2b
Iarley Souza <iarleysouza615@gmail.com>
2025-10-19 13:45:15 -0300

    ajuste s3

--- src/main/java/biblioteca/onliine/biblioteca/infrastructure/aws/S3Service.java
+++ src/main/java/biblioteca/onliine/biblioteca/infrastructure/aws/S3Service.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -14,14 +15,16 @@ import java.io.File;
 @Service
 public class S3Service {
     private final S3Client s3Client;
-
-    @Value("${aws.s3.bucket-name}")
-    private String bucketName;
+    private final String bucketName;
+    private final String region;
 
     public S3Service(
             @Value("${aws.region}") String region,
             @Value("${aws.access-key-id}") String accessKey,
-            @Value("${aws.secret-access-key}") String secretKey) {
+            @Value("${aws.secret-access-key}") String secretKey,
+            @Value("${aws.s3.bucket-name}") String bucketName) {
+        this.bucketName = bucketName;
+        this.region = region;
 
         this.s3Client = S3Client.builder()
                 .region(Region.of(region))
@@ -38,9 +41,14 @@ public class S3Service {
                 .key(caminhoNoBucket)
                 .build();
 
-        s3Client.putObject(request, arquivo.toPath());
+        s3Client.putObject(request, RequestBody.fromFile(arquivo));
 
-        return "https://" + bucketName + ".s3." + s3Client.serviceClientConfiguration().region().id()
-                + ".amazonaws.com/" + caminhoNoBucket;
+        String url;
+        if ("us-east-1".equals(region)) {
+            url = "https://" + bucketName + ".s3.amazonaws.com/" + caminhoNoBucket;
+        } else {
+            url = "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + caminhoNoBucket;
+        }
+        return url;
     }
 }
